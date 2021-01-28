c9cf6db4debc4c9a666be77ae6253ab277e079d8
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-28 20:33:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,9 +20,11 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   emoticon_hash = load_library(file_path)
+  japanese_emoji = ""
   emoticon_hash.each do |name, lang_hash|
     if emoticon == lang_hash[:english] 
         japanese_emoji = lang_hash[:japanese]
+        japanese_emoji
         else "Sorry, that emoticon was not found"
     end
   end

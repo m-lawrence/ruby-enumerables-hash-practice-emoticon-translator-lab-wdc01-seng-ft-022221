4bbe3db77e3daeb72bcfe0a857d10f8efc479b5b
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-28 20:30:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,39 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  library = YAML.load_file(file_path)
+  new_lib = {}
+  library.each do |name, emo_array| 
+    new_lib[name] = {:english => "", :japanese => ""}
+    emo_array.each_with_index do |emoji, index|
+      if index == 0 
+        new_lib[name][:english] = emoji
+        elsif index == 1 
+        new_lib[name][:japanese] = emoji
+      end
+    end
+  end
+  new_lib
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  emoticon_hash = load_library(file_path)
+  emoticon_hash.each do |name, lang_hash|
+    if emoticon == lang_hash[:english] 
+        japanese_emoji = lang_hash[:japanese]
+        else "Sorry, that emoticon was not found"
+    end
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  emoticon_hash = load_library(file_path)
+  emoticon_hash.each do |name, lang_hash|
+    if emoticon == lang_hash[:japanese]
+      emoticon_hash[name]
+    else "Sorry, that emoticon was not found"
+    end
+  end
 end
